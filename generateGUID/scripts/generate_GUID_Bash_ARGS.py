0d734d4f5d2d5b2f956962ff6a4f35069ad30529
David Meunier <david_meunier_79@hotmail.fr>
2020-06-11 11:49:58 +0200

    error (

--- generateGUID/scripts/generate_GUID_Bash_ARGS.py
+++ generateGUID/scripts/generate_GUID_Bash_ARGS.py
@@ -8,7 +8,7 @@ if len(sys.argv) > 1:
     if len(sys.argv) == 5:
         key = sys.argv[1]+sys.argv[2]+sys.argv[3]+sys.argv[4]
         guid=generate_GUID(key)
-        print(guid))
+        print(guid)
     elif len(sys.argv) == 2:
         key = sys.argv[1]
         guid=generate_GUID(key)
