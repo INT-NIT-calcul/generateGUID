697ffb5d404a9c8c77ee1dfbdbbb7e874226cd40
David Meunier <david_meunier_79@hotmail.fr>
2020-07-30 11:46:18 +0200

    no return carriage

--- generateGUID/scripts/generate_GUID_Bash_ARGS.py
+++ generateGUID/scripts/generate_GUID_Bash_ARGS.py
@@ -7,12 +7,12 @@ if len(sys.argv) > 1:
 
     if len(sys.argv) == 5:
         guid=generate_GUID2(sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4])
-        print(guid)
+        print(guid, end = '')
         sys.exit(0)
     elif len(sys.argv) == 2:
         key = sys.argv[1]
         guid=generate_GUID(key)
-        print(guid)
+        print(guid, end = '')
         sys.exit(0)
     else:
         print("mauvais nombre d'arguments: {}".format(len(sys.argv)))
